cdc07af236af5b12b08606c9735cce5d07f440be
Christopher Saez <christophersaez@Krice-MBPr-3.local>
2016-07-02 19:26:29 +0200

    add animation countdown + link map

--- examples/CountDownViewController.m
+++ examples/CountDownViewController.m
@@ -7,8 +7,10 @@
 //
 
 #import "CountDownViewController.h"
-
+#define MAX_COUNTDOWN_IMAGES 100
+#define DURATION 2
 @interface CountDownViewController ()
+@property (retain, nonatomic) IBOutlet UIImageView *countDownImageView;
 
 @end
 
@@ -18,13 +20,22 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 	[self.navigationController setNavigationBarHidden:YES animated:NO];
+	
+	NSMutableArray *images = [NSMutableArray new];
+	for (int i = 0; i < MAX_COUNTDOWN_IMAGES; i++){
+		[images addObject:[UIImage imageNamed:[NSString stringWithFormat:@"decompte_1_%d", i]]];
+	}
+	self.countDownImageView.animationImages = images;
+	self.countDownImageView.animationDuration = DURATION;
+	self.countDownImageView.animationRepeatCount = 1;
+	[self.countDownImageView startAnimating];
+	
+	[self performSelector:@selector(didFinishAnimation) withObject:nil afterDelay:3];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)didFinishAnimation{
+	[self performSegueWithIdentifier:@"ShowMapSegue" sender:nil];
 }
-
 /*
 #pragma mark - Navigation
 
@@ -35,4 +46,8 @@
 }
 */
 
+- (void)dealloc {
+	[_countDownImageView release];
+	[super dealloc];
+}
 @end
